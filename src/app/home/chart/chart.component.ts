c6d11721f37c80eae5e6da6d3d6b27011f0f62dd
Vivian <vivz753@MacaroniBook.local>
2019-04-21 12:42:42 -0700

    write a function to pump live dummy data

--- src/app/home/chart/chart.component.ts
+++ src/app/home/chart/chart.component.ts
@@ -7,33 +7,70 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class ChartComponent implements OnInit {
-  constructor() { }
+  public date = new Date();
+  private setIntervalHandler: any;
+
+  constructor() {
+   }
+
   public barChartOptions = {
     scaleShowVerticalLines: false,
     responsive: true
   };
 
-  public chartLabels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  // public chartLabels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  public chartLabels = [];
   public chartType = 'line';
   public chartLegend = true;
   public chartData = [
-    {data: [
-    ],
-       label: 'Seismic Data', fill: false, lineTension: 0, pointRadius: 0 },
-    // {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
+    {
+      data: [],
+      label: 'Seismic Data', fill: false, lineTension: 0, pointRadius: 0 
+    },
   ];
+  public xyData = [];
+
+  pumpData() {
+    let xy = {
+      x: (Math.random()*100),
+      y: (Math.random()*100)
+    }
+
+
+
+    if(this.xyData.length >= 50 && this.chartLabels.length >=50){
+      this.xyData.shift();
+      this.chartLabels.shift();
+    }
+    let time = (new Date()).toISOString().slice(0, 19).replace(/-/g, "/").replace("T", " ");
+    this.chartLabels.push(time);
+    this.xyData.push(xy);
+    
+
+    let chartData = [
+      {
+        data: [],
+        label: 'Seismic Data', fill: false, lineTension: 0, pointRadius: 0 
+      },
+    ];
+
+    this.xyData.forEach((e) => {
+      chartData[0].data.push(e);
+    })
+    
+  this.chartData = chartData;
+
+  console.log(this.chartData);
+  console.log(this.date.getTime());
+  
+  }
 
   ngOnInit() {
-    for (let i = 0 ; i <200; i++) {
-      let xy = {
-        x: (Math.random()*100),
-        y: (Math.random()*100)
-      }
-    this.chartData[0].data.push(xy);
-          // this.chartData[0].data[i].y = (Math.random()*i);
+    this.setIntervalHandler = setInterval(() => this.pumpData(), 3000);
+  }
 
-    this.chartLabels.push(i + '');
-    }
+  ngOnDestroy() {
+    clearInterval(this.setIntervalHandler);
   }
 
 }
