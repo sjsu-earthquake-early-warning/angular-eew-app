5bfb8b2c5f43a1793662307e7675b7cc05bce9d7
Tai <taidao90@gmail.com>
2019-05-05 20:53:37 -0700

    solved CORS issue

--- expressServices/sendSMS.js
+++ expressServices/sendSMS.js
@@ -9,13 +9,13 @@ AWS.config.update({region: 'us-west-2'});
 
 // Create publish parameters
 
-/*
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
   });
-  */
+
 
 app.get('/sendSMS/add', (req, res) => {
     var params = {
